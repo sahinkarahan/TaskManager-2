c59c81764612d73f6575576cbbf389ab8bbcea36
sahinkarahan <sahin.karahan@hotmail.com>
2024-12-31 11:51:35 +0300

    Initial Commit

---
+++ TaskManager-2/TaskManager_2App.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManager_2App.swift
+//  TaskManager-2
+//
+//  Created by Şahin Karahan on 31.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskManager_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
